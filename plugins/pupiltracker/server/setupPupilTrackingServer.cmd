c2c43976b004c6279fd5b691074c0802b999dfac
Fabian Wildgrube <fabian.wildgrube@gmx.de>
2021-03-31 13:34:43 +0200

    download server source from github repo

--- plugins/pupiltracker/server/setupPupilTrackingServer.cmd
+++ plugins/pupiltracker/server/setupPupilTrackingServer.cmd
@@ -2,19 +2,21 @@
 SETLOCAL ENABLEEXTENSIONS
 SET parent=%~dp0
 
-IF NOT EXIST pupil_tracking-server (
-    IF NOT Exist pupil_tracking-server.zip (
-        ::Download server code. TODO: make reading public or upload to github!
-        wget https://hcai.eu/git/alexanderh/pupil_tracking/-/archive/server/pupil_tracking-server.zip
+SET servercodedir=PupilSizeTracker-server
+
+IF NOT EXIST %servercodedir% (
+    IF NOT Exist server.zip (
+        ::Download server code
+        wget https://github.com/FabianWildgrube/PupilSizeTracker/archive/refs/heads/server.zip
     )
 
     ECHO Unzipping server
-    powershell.exe -nologo -noprofile -command "& { $shell = New-Object -COM Shell.Application; $target = $shell.NameSpace('%parent%'); $zip = $shell.NameSpace('%parent%pupil_tracking-server.zip'); $target.CopyHere($zip.Items(), 16); }"
-    ECHO Unzipped server to %parent%pupil_tracking-server
-    DEL pupil_tracking-server.zip
+    powershell.exe -nologo -noprofile -command "& { $shell = New-Object -COM Shell.Application; $target = $shell.NameSpace('%parent%'); $zip = $shell.NameSpace('%parent%server.zip'); $target.CopyHere($zip.Items(), 16); }"
+    ECHO Unzipped server to %parent%%servercodedir%
+    DEL server.zip
 )
 
-cd pupil_tracking-server
+cd %servercodedir%
 
 SET imagetag=pupiltrackingserver
 SET containerName=pupiltrackingserver
