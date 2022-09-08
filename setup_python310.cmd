b14f84c2e41cc58cf02b57772955162a051f9531
Tobias Baur <baur@hcm-lab.de>
2022-09-08 10:32:14 +0200

    fixed paths

--- setup_python310.cmd
+++ setup_python310.cmd
@@ -5,6 +5,7 @@
 set PYTHON_VER_FULL=3.10.6
 set PYTHON_VER=310
 
+set SRC=https://github.com/hcmlab/ssi/raw/master/bin/x64/vc140/
 set DST=.\bin\x64\vc140\
 set LIB=.\libs\build\
 
@@ -14,9 +15,9 @@ set LIB=.\libs\build\
 %DST%7za.exe x %DST%python-%PYTHON_VER_FULL%-embed-amd64.zip -aoa -o%DST%
 %DST%7za.exe x %DST%python%PYTHON_VER%.zip -aoa -o%DST%python%PYTHON_VER%
 %DST%7za.exe x %LIB%python-%PYTHON_VER_FULL%.zip -aoa -o%LIB%
-move %LIB%\cpython-%PYTHON_VER_FULL% %LIB%\Python-%PYTHON_VER_FULL% 
+move %LIB%\cpython-%PYTHON_VER_FULL% %LIB%\Python-%PYTHON_VER_FULL% ´
 
-(echo . & echo .\ & echo .\DLLs & echo .\lib & echo .\lib\plat-win & echo .\lib\site-packages & echo .\python%PYTHON_VER%) > %DST%python%PYTHON_VER%._pth
+%DST%wget.exe -q %SRC%/python%PYTHON_VER%._pth -O %DST%python%PYTHON_VER%._pth
 %DST%wget.exe -q https://bootstrap.pypa.io/get-pip.py -O %DST%get-pip.py
 %DST%python %DST%get-pip.py
 %DST%python -m pip install numpy
